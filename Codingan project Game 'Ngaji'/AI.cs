79b2c3c8b0c96a93c1ede22ed84c379659890cf5
Seta Murdha P <murdhapamungkas@gmail.com>
2020-11-28 01:32:08 +0700

    new file:   Codingan project Game 'Ngaji'/AI.cs 	new file:   Codingan project Game 'Ngaji'/AI.cs.meta 	new file:   Codingan project Game 'Ngaji'/AIFix.cs 	new file:   Codingan project Game 'Ngaji'/AIFix.cs.meta 	new file:   Codingan project Game 'Ngaji'/CatDuduk.controller 	new file:   Codingan project Game 'Ngaji'/CatDuduk.controller.meta 	new file:   Codingan project Game 'Ngaji'/Enemy.cs 	new file:   Codingan project Game 'Ngaji'/Enemy.cs.meta 	new file:   Codingan project Game 'Ngaji'/Ext Asset.meta 	new file:   Codingan project Game 'Ngaji'/Menu1.png 	new file:   Codingan project Game 'Ngaji'/Menu1.png.meta 	new file:   Codingan project Game 'Ngaji'/Movement.controller 	new file:   Codingan project Game 'Ngaji'/Movement.controller.meta 	new file:   Codingan project Game 'Ngaji'/Movement.cs 	new file:   Codingan project Game 'Ngaji'/Movement.cs.meta 	new file:   Codingan project Game 'Ngaji'/New Terrain.asset 	new file:   Codingan project Game 'Ngaji'/New Terrain.asset.meta 	new file:   Codingan project Game 'Ngaji'/PauseMenu.cs 	new file:   Codingan project Game 'Ngaji'/PauseMenu.cs.meta 	new file:   Codingan project Game 'Ngaji'/Scenes.meta 	new file:   Codingan project Game 'Ngaji'/ScriptAI.cs 	new file:   Codingan project Game 'Ngaji'/ScriptAI.cs.meta 	new file:   Codingan project Game 'Ngaji'/StartMenu.cs 	new file:   Codingan project Game 'Ngaji'/StartMenu.cs.meta 	new file:   Codingan project Game 'Ngaji'/TPCamera.cs 	new file:   Codingan project Game 'Ngaji'/TPCamera.cs.meta 	new file:   Codingan project Game 'Ngaji'/TriggerScene.cs 	new file:   Codingan project Game 'Ngaji'/TriggerScene.cs.meta 	new file:   Codingan project Game 'Ngaji'/UjiInteract.cs 	new file:   Codingan project Game 'Ngaji'/UjiInteract.cs.meta 	new file:   Codingan project Game 'Ngaji'/UjiMCharPulang - Copy - Copy (2).fbx 	new file:   Codingan project Game 'Ngaji'/UjiMCharPulang - Copy - Copy (2).fbx.meta 	new file:   Codingan project Game 'Ngaji'/Untitled-1.jpg 	new file:   Codingan project Game 'Ngaji'/Untitled-1.jpg.meta

---
+++ Codingan project Game 'Ngaji'/AI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+public class AI : MonoBehaviour
+{
+    public GameObject target;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        this.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = target.transform.position;
+    }
+}
---
+++ Codingan project Game 'Ngaji'/Movement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 8f;
+    [SerializeField] private float runSpeed = 16f;
+    private float currentSpeed = 0f;
+    private float speedSmoothVelocity = 0f;
+    private float speedSmoothTime = 0.1f;
+    private float rotationSpeed = 0.1f;
+    private float gravity = 3f;
+
+    private Rigidbody rb;
+
+    private Transform mainCameraTransform = null;
+
+    private CharacterController controller = null;
+    private Animator animator = null;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+        rb = GetComponent<Rigidbody>();
+
+        mainCameraTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        move();
+    }
+
+    private void move()
+    {
+
+        Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        Vector3 forward = mainCameraTransform.forward;
+        Vector3 right = mainCameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 desiredMoveDirection = (forward * movementInput.y + right * movementInput.x).normalized;
+        Vector3 gravityVector = Vector3.zero;
+        
+        if (!controller.isGrounded)
+        {
+            gravityVector.y -= gravity;
+        }
+
+        if(desiredMoveDirection != Vector3.zero)
+        {
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), rotationSpeed);
+
+        }
+
+        float targetSpeed = walkSpeed * movementInput.magnitude;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            targetSpeed = runSpeed * movementInput.magnitude;
+        }
+
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        controller.Move(desiredMoveDirection * currentSpeed * Time.deltaTime);
+
+        controller.Move(gravityVector * Time.deltaTime);
+
+        animator.SetFloat("Mvment", 0.4f * movementInput.magnitude, speedSmoothTime, Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetFloat("Mvment", 1f * movementInput.magnitude, speedSmoothTime, Time.deltaTime);
+        }
+    }
+
+}
